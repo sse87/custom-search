c55e74950c17587deb2b0083def4061edf598698
Sigurður Snær Eiríksson <sse1987@gmail.com>
2019-01-29 18:56:42 +0000

    Added bootstrap and axios packages and made Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import Header from './components/Header'
 
 class App extends Component {
   render () {
     return (
-      <div>
-        Hello World<br />
-        <strong>Custom Search Demo</strong> here!
-      </div>
+      <Fragment>
+        <Header />
+        <main>
+          <div className='container'>
+            <div>
+              Hello World<br />
+              <strong>Custom Search Demo</strong> here!
+            </div>
+          </div>
+        </main>
+      </Fragment>
     )
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='navbar navbar-dark bg-dark mb-3'>
+      <div className='container'>
+        <span className='navbar-brand'>Custom Search Demo</span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
