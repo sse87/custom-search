96d762521f259cf6fe22e24a0eeafd78316caaf5
Sigurður Snær Eiríksson <sse1987@gmail.com>
2020-08-17 16:31:03 +0000

    Add config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+
+export const googleapisConfig = {
+  url: 'https://content.googleapis.com/customsearch/v1',
+  params: {
+    cx: '001361074102112665899:p7mybnrloug',
+    key: 'AIzaSyAj6qgbKJfP5qpY7fXJ7JAjlLb-vdwE7fc',
+    searchType: 'image',
+    q: ''
+  }
+}
